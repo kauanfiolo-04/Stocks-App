566b5665c2edd741ca9251d14efa4e84ce99dfef
kauanfiolo-04 <kauanfiolo@hotmail.com>
2025-11-04 17:49:18 -0300

    feat: adding InputField component

--- stocks-app/app/(auth)/sign-up/page.tsx
+++ stocks-app/app/(auth)/sign-up/page.tsx
@@ -1,6 +1,58 @@
+"use client";
+
+import InputField from "@/components/forms/InputField";
+import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+
 const SignUp = () => {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors, isSubmitting }
+  } = useForm<SignUpFormData>({
+    defaultValues: {
+      fullName: "",
+      email: "",
+      password: "",
+      country: "US",
+      investmentGoals: "Growth",
+      riskTolerance: "Medium",
+      preferredIndustry: "Technology"
+    }, mode: "onBlur"
+  });
+
+  const onSubmit = async (data: SignUpFormData) => {
+    try {
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div>SignUp</div>
+    <>
+      <h1 className="form-title">SignUp & Personalize</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+        <InputField 
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLenght: 2 }}
+        />
+
+        <Button 
+          className="yellow-btn w-full mt-5"
+          type="submit" 
+          disabled={isSubmitting}
+        >
+          {isSubmitting ? "Creating account" : "Start Yout Investing Journey"}
+        </Button>
+      </form>
+    </>
   );
 };
 
