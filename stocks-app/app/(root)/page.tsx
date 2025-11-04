88a9c68d909b4c81ad0946c1da7c0f1af998b1ec
kauanfiolo-04 <kauanfiolo@hotmail.com>
2025-11-04 15:04:59 -0300

    Layout and Header created

---
+++ stocks-app/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+const Page = () => {
+  return (
+    <div className="flex min-h-screen home-wrapper">
+      Home
+    </div>
+  );
+};
+
+export default Page;
---
+++ stocks-app/components/Header.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import NavItems from "./NavItems";
+import UserDropdown from "./UserDropdown";
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+      <div className="container header-wrapper">
+        <Link href="/">
+          <Image 
+            className="h-8 w-auto cursor-pointer"
+            src="/assets/icons/logo.svg" 
+            alt="Stocks App Logo" 
+            width={140}
+            height={32}
+          />
+        </Link>
+
+        <nav className="hidden sm:block">
+          <NavItems />
+        </nav>
+
+        <UserDropdown />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
