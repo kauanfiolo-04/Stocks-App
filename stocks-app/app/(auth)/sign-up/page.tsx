0f05fd83d0cc2fc3abcadc2e5854cb91cd3a588c
kauanfiolo-04 <kauanfiolo@hotmail.com>
2025-11-05 08:57:26 -0300

    SelectField Created

--- stocks-app/app/(auth)/sign-up/page.tsx
+++ stocks-app/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import InputField from "@/components/forms/InputField";
+import SelectField from "@/components/forms/SelectField";
 import { Button } from "@/components/ui/button";
+import { INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS } from "@/lib/constants";
 import { useForm } from "react-hook-form";
 
 const SignUp = () => {
@@ -44,12 +46,71 @@ const SignUp = () => {
           validation={{ required: "Full name is required", minLenght: 2 }}
         />
 
+        <InputField 
+          name="fullName"
+          label="Full Name"
+          placeholder="John Doe"
+          register={register}
+          error={errors.fullName}
+          validation={{ required: "Full name is required", minLenght: 2 }}
+        />
+
+        <InputField 
+          name="email"
+          label="Email"
+          placeholder="john@doe.com"
+          register={register}
+          error={errors.email}
+          validation={{ required: "Email is required",pattern: /^\w+@\w+\.\w+$/, message: "Email addres is required" }}
+        />
+
+        <InputField 
+          name="password"
+          label="Password"
+          placeholder="Enter a strong password"
+          register={register}
+          error={errors.password}
+          validation={{ required: "Password is required", minLenght: 8 }}
+        />
+
+        {/* Country */}
+
+        <SelectField 
+          name="investmentGoals"
+          label="Investment Goals"
+          placeholder="Select your investment goal"
+          options={INVESTMENT_GOALS}
+          control={control}
+          error={errors.investmentGoals}
+          required
+        />
+
+        <SelectField 
+          name="riskTolerance"
+          label="Risk Tolerance"
+          placeholder="Select your risk tolerance"
+          options={RISK_TOLERANCE_OPTIONS}
+          control={control}
+          error={errors.riskTolerance}
+          required
+        />
+
+        <SelectField 
+          name="preferredIndustry"
+          label="Preferred Industry"
+          placeholder="Select your preferred industry"
+          options={PREFERRED_INDUSTRIES}
+          control={control}
+          error={errors.preferredIndustry}
+          required
+        />
+
         <Button 
           className="yellow-btn w-full mt-5"
           type="submit" 
           disabled={isSubmitting}
         >
-          {isSubmitting ? "Creating account" : "Start Yout Investing Journey"}
+          {isSubmitting ? "Creating account" : "Start Your Investing Journey"}
         </Button>
       </form>
     </>
