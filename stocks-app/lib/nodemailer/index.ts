62e2e550477746d5f6fa46a357ed250082463908
kauanfiolo-04 <kauanfiolo@hotmail.com>
2025-12-05 16:02:34 -0300

    Implement Daily news summary via email

--- stocks-app/lib/nodemailer/index.ts
+++ stocks-app/lib/nodemailer/index.ts
@@ -1,5 +1,5 @@
 import nodemailer from "nodemailer";
-import { WELCOME_EMAIL_TEMPLATE } from "./templates";
+import { NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from "./templates";
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
@@ -19,6 +19,24 @@ export const sendWelcomeEmail = async ({ email, name, intro }: WelcomeEmailData)
     to: email,
     subject: "Welcome to StocksApp - your stock market toolkit is ready!",
     text: "Thanks for joining Stocks App",
+    html: htmlTemplate
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+export const sendNewsSummaryEmail = async (
+  { email, date, newsContent }: { email: string, date: string, newsContent: string }
+) => {
+  const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
+    .replace("{{date}}", date)
+    .replace("{{newsContent}}", newsContent);
+
+  const mailOptions = {
+    from: `"Stocks App <kauanfiolo@gmail.com>"`,
+    to: email,
+    subject: `Market News Summary Today - ${date}`,
+    text: `Today's market news summary from Stocks App`,
     html: htmlTemplate
   };
 
